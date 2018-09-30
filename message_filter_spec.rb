d56d60a6cb7c0b4b2ccff4f32bdd084a4e909ad1
komoriyuichiro <thekomori1113@gmail.com>
2018-10-01 03:25:07 +0900

    Join lines

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -2,11 +2,6 @@ require_relative 'message_filter'
 
 describe MessageFilter, 'with argument "foo"' do
   subject { MessageFilter.new('foo') }
-  it {
-    is_expected.to be_detect('hello from foo')
-  }
-
-  it {
-    is_expected.to_not be_detect('hello, world')
-  }
+  it { is_expected.to be_detect('hello from foo') }
+  it { is_expected.to_not be_detect('hello, world') }
 end
